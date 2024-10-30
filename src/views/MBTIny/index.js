f9536bfa66bdaa42dc7e97ce69aba8d8bec813bf
hyewonbae <bhwn98@gmail.com>
2024-10-30 21:43:11 +0900

    [feature/7] 추천 화면 구성

--- src/views/MBTIny/index.js
+++ src/views/MBTIny/index.js
@@ -12,7 +12,8 @@ import { useNavigate, useLocation } from 'react-router-dom';
 export default function MBTInyMain() {
     const [accessToken, setAccessToken] = useState('');
     const [userName, setUserName] = useState('');
-    const [friendRecommd, setFriendRecommdData] = useState([]); // 추천 도서 목록을 위한 상태 배열로 초기화
+    const [firstRecommd, setFirstRecommdData] = useState([]); // 우리 아이가 좋아한 도서와 유사한 도서
+    const [secondRecommd, setSecondRecommdData] = useState([]); // 우리 아이와 비슷한 성향의 친구들이 추천한 도서
     const location = useLocation();
     const childId = location.state?.child_id || localStorage.getItem("childId");
 
@@ -71,11 +72,22 @@ export default function MBTInyMain() {
         const token = accessToken || localStorage.getItem("jwtToken");
 
         try {
-            // 첫 번째 API - 비슷한 성향의 친구들이 추천한 도서 목록
-            const friendRecommdDataResponse = await axios.get(`${API_DOMAIN}/child/${childId}/embedding`, {
+            // // 첫번째 API - 자녀가 좋아요한 도서와 유사한 도서 추천
+            // const firstRecommdDataResponse = await axios.get(`${API_DOMAIN}/contents/child/${childId}/recommendations`, {
+            //     headers: { Authorization: `Bearer ${token}` }
+            // });
+            // setFirstRecommdData(firstRecommdDataResponse.data.result || []);
+
+
+            // 두번째 API - 비슷한 성향의 친구들이 추천한 도서 목록
+            const secondRecommdDataResponse = await axios.get(`${API_DOMAIN}/child/${childId}/embedding`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
-            setFriendRecommdData(friendRecommdDataResponse.data.result || []);
+            setSecondRecommdData(secondRecommdDataResponse.data.result || []);
+
+
+
+
         } catch (error) {
             console.error("Error fetching child data:", error);
         }
@@ -84,13 +96,32 @@ export default function MBTInyMain() {
     return (
         <div>
             <Header />
+            <div className="recommd-container">
+
+            <div className="recommd-main-container">
+                <div className="recommd-content-container">
+                    <div className="recommended-books">
+                        <h3>우리 아이가 좋아한 도서와 유사한 도서</h3>
+                        <p>- 연령, 성별, 성향을 기반하여 맞춤형 도서를 추천합니다.</p>
+                        <div className="book-container" ref={scrollRef}>
+                            {firstRecommd.map((book, index) => (
+                                <div className="book-item" key={index}>
+                                    <img src={book.profileUrl || "../img/avatar.png"} alt={book.title} className="book-cover" />
+                                    <p className="book-title" title={book.title}>{book.title || "제목 없음"}</p>
+                                </div>
+                            ))}
+                        </div> 
+                    </div>
+                </div>
+            </div>
+
             <div className="recommd-main-container">
                 <div className="recommd-content-container">
                     <div className="recommended-books">
                         <h3>우리 아이와 비슷한 성향의 친구들이 추천한 도서</h3>
                         <p>- 자녀의 연령, 성별, MBTI 정보를 바탕으로 맞춤형 도서를 추천합니다.</p>
                         <div className="book-container" ref={scrollRef}>
-                            {friendRecommd.map((book, index) => (
+                            {secondRecommd.map((book, index) => (
                                 <div className="book-item" key={index}>
                                     <img src={book.profileUrl || "../img/avatar.png"} alt={book.title} className="book-cover" />
                                     <p className="book-title" title={book.title}>{book.title || "제목 없음"}</p>
@@ -99,6 +130,7 @@ export default function MBTInyMain() {
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
             <NavBar />
         </div>

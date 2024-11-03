89734ffd35aa5e1063189754806ae86ce2e3d559
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-11-03 13:23:59 +0900

    [fix] 서버 도메인 변경

--- src/api/domain.js
+++ src/api/domain.js
@@ -1,5 +1,5 @@
-// export const API_DOMAIN = 'http://localhost:8080/api/v1';
-export const API_DOMAIN = 'http://52.78.195.10:8080/api/v1';
+export const API_DOMAIN = 'http://localhost:8080/api/v1';
+// export const API_DOMAIN = 'http://52.78.195.10:8080/api/v1';
 
 // export const CLIENT_DOMAIN = "http://localhost:3000"; // 로컬
 export const CLIENT_DOMAIN = "https://mbtiny.netlify.app";
